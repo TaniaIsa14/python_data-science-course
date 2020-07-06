3192530dc8199557c55119ff207ba5ec623d9fb8
Tania Isa <34392109+TaniaIsa14@users.noreply.github.com>
2020-07-06 13:58:45 +0600

    Add files via upload

---
+++ hm2.py
@@ -0,0 +1,10 @@
+n=int(input('Value:')
+count_ev=0
+count_od=0
+for n in range(1,n+1):
+    if n%2==0:
+         count_ev = count_ev + 1
+       print('even:',count_ev)
+     else:
+    count_od = count_od + 1
+       print('odd',count_od)
