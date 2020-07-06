36bd1a8d82ea21b0436bde873ac6d18ce8d95cf5
Tania Isa <34392109+TaniaIsa14@users.noreply.github.com>
2020-07-06 14:03:57 +0600

    Add files via upload

---
+++ dataf5.py
@@ -0,0 +1,9 @@
+import pandas as pd
+my_fvrt=[
+            {'kacci','hindi','chokerbali'},
+            {'pulo','bangla','nishirat'},
+    {'birani','robindro','gpyenda'}
+
+]
+frvt_dict=pd.DataFrame(data=my_fvrt,columns=['Food','Song','Book'])
+print(frvt_dict)
